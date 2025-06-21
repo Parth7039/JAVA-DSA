fa5c8acf751590c29dd6469d339dc786576f274d
Parth7039 <p.k.bhamare07@gmail.com>
2025-06-21 21:21:29 +0530

    leetcode

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        String first = "parth";
-        String last = "bhamare";
-        String fullname = first +" "+ last; //parth bhamare
-        System.out.println(fullname.toUpperCase());
-        if (first.compareTo(last) == 1){
-            System.out.println("match");
-        } else {
-            System.out.println("no match");
+    public static LinkedList mergeTwoLists(LinkedList<Integer> list1, LinkedList<Integer> list2) {
+        LinkedList<Integer> list3 = new LinkedList<>();
+        for (int i=0; i<list1.size(); i++){
+            int val = (int) list1.get(i);
+            list3.add(val);
         }
+        for (int i=0; i<list2.size(); i++){
+            int val = (int) list2.get(i);
+            list3.add(val);
+        }
+        Collections.sort(list3);
+        return list3;
+    }
+    public static void main(String[] args) {
+        LinkedList<Integer> list1 = new LinkedList<>(Arrays.asList(1, 2, 4));
+        LinkedList<Integer> list2 = new LinkedList<>(Arrays.asList(1, 3, 4));
+
+        LinkedList<Integer> merged = mergeTwoLists(list1, list2);
+        System.out.println("Merged and sorted list: " + merged);
     }
 }
