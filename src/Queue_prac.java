299be4385d2a4861e5a210ec36cbea009aa7f460
Parth7039 <p.k.bhamare07@gmail.com>
2025-06-25 22:28:47 +0530

    Queue

--- src/Queue_prac.java
+++ src/Queue_prac.java
@@ -2,10 +2,15 @@ public class Queue_prac {
     static class Queue{
         static int arr[];
         static int size;
+        static int rear = -1;
 
         Queue(int size) {
             arr = new int[size];
             this.size = size;
+        }
+
+        public static boolean isEmpty() {
+            return rear == -1;
         }
     }
     public static void main(String[] args) {
