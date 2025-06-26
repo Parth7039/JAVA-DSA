50361fbf18e3c0aae7c7a64657683f3567295b95
Parth7039 <p.k.bhamare07@gmail.com>
2025-06-26 21:24:41 +0530

    Queue

--- src/Queue_prac.java
+++ src/Queue_prac.java
@@ -12,8 +12,46 @@ public class Queue_prac {
         public static boolean isEmpty() {
             return rear == -1;
         }
+
+        public static void add(int data) {
+            if (rear == size-1) {
+                System.out.println("Queue is full");
+                return;
+            }
+            rear++;
+            arr[rear] = data;
+            System.out.println("Successfully added "+data);
+        }
+
+        public static int remove(){
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            int front = arr[0];
+            for (int i = 0; i < rear; i++) {
+                arr[i] = arr[i+1];
+            }
+            rear--;
+            return front;
+        }
+
+        public static int peek() {
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            int front = arr[0];
+            return front;
+        }
     }
     public static void main(String[] args) {
-
+        Queue q = new Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        System.out.println(q.peek());
     }
 }
